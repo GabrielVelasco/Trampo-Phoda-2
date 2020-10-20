736c6a2a63fbd042161102943dedf0dad62c5c18
BigPeter <62038609+Big-Peter@users.noreply.github.com>
2020-10-20 18:36:06 -0300

    Exercicio 3 atualizado

---
+++ 3/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "tad.h"
+#define max_veic 10
+#define max_box 6
+int main()
+{
+    fila f[max_box];
+    char placa[10],op;
+    int i,j,tam,box;
+    //-----------Criacao das filas------------------------
+    for(i = 0;i < max_box;i++){
+        f[i] = cria_fila();
+        if(f[i] == NULL){
+            printf("Erro ao alocar as filas\n");
+            return 0;
+        }
+    }
+    //----------------------------------------------------
+    printf("1-Entrada de veic.       2-Saida de veic.\n");
+    printf("3-Visualiza             4-Sair");
+    //----------------------------------------------------
+    while(1){
+        printf("\nEscolha uma opcao valida:");
+        setbuf(stdin,NULL);
+        op = getchar();
+        switch(op){
+         case '1':
+             printf("Qual placa deseja inserir?\n");
+             setbuf(stdin,NULL);
+             gets(placa);
+             tam = tamanho(f[0]);
+             for(i = 1;i < max_box-1; i++){
+                 if(tam > tamanho(f[i])){
+                     tam = tamanho(f[i]);
+                     box = i;
+             }
+            }
+            if(insere(f[box],placa)){
+                printf("Inserido com sucesso no Box [%d]",box);
+            }
+            else{
+                if(insere(f[max_box-1],placa))
+                    printf("Inserido na fila de espera\n");
+                else
+                    printf("Todos os boxes e a fila de espera estao cheios\n");
+            }
+            break;
+//----------------------------------------------------------------------
+         case '2':
+            printf("Em qual box deseja retirar seu veiculo?\n");
+            scanf("%d",&box);
+            printf("Qual placa deseja remover?\n");
+            setbuf(stdin,NULL);
+            gets(placa);
+            if(remover(f[box],placa)){
+             if(tamanho(f[max_box-1])){
+                insere_da_espera(f[box],f[max_box-1]);
+                remove_ini(f[max_box-1]);
+                printf("[%s] Foi removido com sucesso e a fila de espera andou\n",placa);
+                }
+             else
+                printf("[%s] Foi removido com sucesso\n",placa);
+                }
+            else
+                printf("Falha ao remover\n");
+            break;
+//----------------------------------------------------------------------
+         case '3':
+            for(i = 0; i < max_box;i++){
+                tam = tamanho(f[i]);
+            if(i < 5)
+               printf("Box [%d]:\n",i);
+            else
+               printf("Fila de espera:\n");
+              for(j = 0;j < tam;j++){
+                visualizar(f[j]);
+             }
+            }
+            break;
+//----------------------------------------------------------------------
+         case '4':
+            printf("Saindo");
+            return 0;
+//----------------------------------------------------------------------
+        }
+       }
+    return 0;
+}
