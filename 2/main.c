8c349c7c768530930eacd4ea6a3ece8d6ddec829
GabrielVelasco <themrgabriel100@gmail.com>
2020-10-20 16:02:29 -0300

    ex 2 e 3

--- 2/main.c
+++ 2/main.c
@@ -1,82 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
 #include "stack.h"
 
+/*
+    Para entender melhor o funcionamento das funoces _solve_post_fix()
+    _infix_to_post() e _is_valid() recomendo ver o video sobre pilha do prof.
+    Entendi que o usuario realizaria uma sequencia de 3 passos, validacao, conversao
+    e avaliacao/resolucao da expressao de entrada, caso o usuario queira mudar
+    fazer apenas uma coisa ou outra basta mudar o app atentando-se que uma 
+    funcao as vezes vai depender da outra, ex; _solve..() depende da _infix_to..().
+
+    Como eh usado a math.h caso for compilar no terminal tem que linkar
+    usando: gcc -o exec main.c stack.c -lm
+
+    Na parte da resolucao da expressao eh pedido os valores dos literais de 0 a 9
+    este processo poderia ser feito dentro da funcao _solve_post_fix().
+    
+*/
+
+void _print_elem(char c_data, float f_data, int key){
+    if(key)
+        printf("[%.2f] ", f_data);
+    else
+        printf("[%c] ", c_data);
+}
+
+void _empty_msg(){
+    printf("Stack is empty!\n");
+}
+
+void _get_value(char exp[]){
+    for(int i = 0; exp[i] != '\0'; i++){
+        if(exp[i] >= 'A' && exp[i] <= 'J'){
+            getchar();
+            printf("Digite valor de [%c] (0 a 9): ", exp[i]);
+            scanf("%c", &exp[i]);
+        }
+    }
+}
+
 int main(){
-    int opc, ch = 0, data;
+    int ch = 0;
+    float r;
+    char exp[50] = {0}; /// inicializa com strlen == 0
     Stack S = _create_stack(); // S aponta para o TOPO da stack
 
-    char exp[50];
     while(1){
+        printf("\nDigite uma expressao infixa ou '0' para sair: ");
         scanf("%s", exp);
-        printf("\nEXP: %s\n", exp);
+        if(exp[0] == '0')
+            break;
         ch = _is_valid(&S, exp);
-        if(ch == 1)
-            printf("Expressao valida!\n");
+        if(ch == 1){
+            printf("Expressao valida, convertendo para pos-fixa...\n");
+            _infix_to_post(&S, exp);
+
+            printf("\nPost-fix: %s\n", exp);
+            printf("\nAtribuir valores:\n");
+            _get_value(exp);
+
+            ch = _solve_post_fix(&S, exp, &r);
+            if(ch == 1){
+                printf("Resultado: %.2f\n", r);
+            }else if(ch == 0){
+                printf("Numero de operandos nao eh adequado!\n");
+            }else if(ch == -1){
+                printf("Numero de operadores nao eh adequado!\n");
+            }else if(ch == 2)
+                printf("Nao pode dividir por 0!\n");
+
+        }else if(ch == 0)
+            printf("Erro de precedencia!\n");
         else if(ch == -1)
             printf("Erro de ordem!\n");
-        else
-            printf("Erro de precedencia!\n");
+
 
         _clean(&S);
     }
-
-    // while(1){
-    //     printf("\n======================MENU=========================================\n");
-    //     printf("[0] Create Stack\n");
-    //     printf("[1] Push top\n");
-    //     printf("[2] Pop top\n");
-    //     printf("[3] Print stack\n");
-    //     printf("[4] Get top\n");
-    //     printf("\nChoose an option: ");
-    //     scanf("%d", &opc);
-
-
-    //     switch(opc){
-    //         case 0:
-    //             S = _create_stack(); // cria lista vazia (sem nodes)
-    //             printf("\nStack created\n");
-    //             break;
-
-    //         case 1:
-    //             printf("\nPush Back, enter a data:\n");
-    //             scanf("%d", &data);
-    //             if(_push(&S, data))
-    //                 printf("Success!\n");
-    //             else
-    //                 printf("Something went wrong!\n");
-    //             break;
-
-    //         case 2:
-    //             if(_pop(&S, &data))
-    //                 printf("%d removed!\n", data);
-    //             else
-    //                 printf("Something went wrong!\n");
-    //             break;
-
-    //         case 3:
-    //             if(!ch || _empty_stack(S))
-    //                 printf("Empty stack or not initialized yet\n");
-    //             else
-    //                 _print(S);
-    //             break;
-
-    //         case 4:
-    //             if(_get_top(S, &data))
-    //                 printf("Top: %d\n", data);
-    //             else
-    //                 printf("Error while consulting Top!\n");
-    //             break;
-
-    //         default:
-    //             ch = 2;
-
-    //             break;
-    //     }
-
-    //     if(!ch) ch = 1;
-    //     if(ch == 2) break;
-    // }
 
     return 0;
 }
--- 2/stack.c
+++ 2/stack.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
 #include "stack.h"
 
 struct node{
-    char data;
+    int key; /// se 1 node armazena int, 0 armazena char.
+    void* data;
     struct node* next;
 
 };
@@ -22,23 +26,45 @@ int _empty_stack(Stack S){
     else return 0;
 }
 
-int _push(Stack *S, char data){
+int _push(Stack *S, char data, float f_data, int key){
+    /// se key == 1, vou add um float no node
+    /// se nao, vou add um char
     Stack node = (Stack) malloc(sizeof(struct node));
     _alloc_check(node, "Error while allocating!");
 
-    node->data = data;
+    node->key = key;
+    if(key){
+        float* p = (float*) malloc(sizeof(float));
+        _alloc_check(p, "Error while allocating float pointer!");
+        *p = f_data;
+        node->data = p;
+    }else{
+        char* p = (char*) malloc(sizeof(int));
+        _alloc_check(p, "Error while allocating char pointer!");
+        *p = data;
+        node->data = p;
+    }
+    //node->data = data;
     node->next = *S; // add no comeco (topo da stack)
     *S = node; // aponta p/ comeco(topo da stack)
 
     return 1;
 }
 
-int _pop(Stack *S, char *data){
+int _pop(Stack *S, char *c_data, float *f_data){
     if(_empty_stack(*S)) return 0;
 
     Stack tmp = *S;
-    *data = tmp->data;
+    if(tmp->key){
+        /// node armazena um float
+        *f_data = *((float*)tmp->data);
+    }else{
+        /// node armazena um char
+        *c_data = *((char*)tmp->data);
+    }
+    //*data = tmp->data;
     *S = tmp->next;
+    free(tmp->data);
     free(tmp);
 
     return 1;
@@ -47,28 +73,24 @@ int _pop(Stack *S, char *data){
 int _get_top(Stack *S, char *data){
     if(_empty_stack(*S)) return 0;
 
-    *data = (*S)->data;
+    *data = *((char*)(*S)->data);
     return 1;
-}
-
-int _preced_par(char data){
-    /// calcula precedencia dos delimitadores de escopo
-    if(data == '(') return 3;
-    else if(data == '[') return 2;
-    else if(data == '{') return 1;
-    else return 0;
 }
 
 int _preced_op(char op){
     /// calcula precedencia dos operadores
-    
+    if(op == '^') return 3;
+    else if(op == '*' || op == '/') return 2;
+    else if(op == '+' || op == '-') return 1;
+    else return 0;
 }
 
 void _clean(Stack *S){
     Stack tmp = NULL; /// aponta para topo da stack
     while(*S != NULL){
-        tmp = *S; /// aponta para topo da stack
+        tmp = *S; /// aponta para topo da stack, node a ser deletado
         *S = (*S)->next; /// aponta para o node de baixo (novo topo)
+        free(tmp->data);
         free(tmp);
     }
 }
@@ -78,30 +100,34 @@ int _is_valid(Stack *S, char exp[]){
     /// return 0 se exp tem erro de precedencia
     /// return -1 se exp tem erro de ordem
     char data;
+    float tmp;
     for(int i = 0; exp[i] != '\0'; i++){
-        /// no decorrer da exp a precedencia tem q ir "aumentando", logo
-        /// se exp[i] for abertura vou empilhar exp[i] se stack estiver vazia OU
-        /// precedencia de exp[i] for >= a precedencia de data (elemento do topo da stack)
+        /// se exp[i] for abertura vou empilha-lo SE stack estiver vazia OU
+        /// se a precedencia (valor do caractere '(', '[' ou '{' na tabela ascii) 
+        /// de exp[i] for <= a precedencia de data (elemento do topo da stack)
         /// caso contrario tenho erro de precedencia
         if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{'){
-            if(!_get_top(S, &data) || _preced_par(exp[i]) >= _preced_par(data))
-                _push(S, exp[i]);
+            if(!_get_top(S, &data) || exp[i] <= data)
+                _push(S, exp[i], 0, 0);
             else{
                 return 0;
             }
 
         }else if(exp[i] == ')'){
-            if(!_pop(S, &data) || data != '('){
+            /// se exp[i] == fechamento, desempilhar elemento do topo (data)
+            /// se data (elemento do topo) != abertura, ou se stack estiver vazia, tenho um erro de ordem
+            /// obs. abertura e fechamento de que ser do mesmo tipo.
+            if(!_pop(S, &data, &tmp) || data != '('){
                 return -1;
             }
 
         }else if(exp[i] == ']'){
-            if(!_pop(S, &data) || data != '['){
+            if(!_pop(S, &data, &tmp) || data != '['){
                 return -1;
             }
 
         }else if(exp[i] == '}'){
-            if(!_pop(S, &data) || data != '{'){
+            if(!_pop(S, &data, &tmp) || data != '{'){
                 return -1;
             }
         }
@@ -113,14 +139,116 @@ int _is_valid(Stack *S, char exp[]){
         return -1;
 }
 
-int _infix_to_post(){
+void _infix_to_post(Stack *S, char exp[]){
+    /// eh garantido que a expressao aqui eh valida dado a sequencia de execucao das funcoes
+    /// propostas no ex2.
+    char ans[50], data; /// ans guarda exp pos-fixa
+    int j = 0;
+    float tmp;
+    for(int i = 0; exp[i] != '\0'; i++){
+        if(exp[i] == '(' || exp[i] == '[' || exp[i] == '{')
+            _push(S, exp[i], 0, 0);
 
+        else if(exp[i] == '^'){
+            while(_get_top(S, &data) && (_preced_op(data) >= _preced_op(exp[i]))){
+                ans[j] = data;
+                j++;
+                _pop(S, &data, &tmp);
+            }
+            _push(S, exp[i], 0, 0);
+        
+        }else if(exp[i] == '*' || exp[i] == '/'){
+            while(_get_top(S, &data) && (_preced_op(data) >= _preced_op(exp[i]))){
+                ans[j] = data;
+                j++;
+                _pop(S, &data, &tmp);
+            }
+            _push(S, exp[i], 0, 0);
+        
+        }else if(exp[i] == '-' || exp[i] == '+'){
+            while(_get_top(S, &data) && (_preced_op(data) >= _preced_op(exp[i]))){
+                ans[j] = data;
+                j++;
+                _pop(S, &data, &tmp);
+            }
+            _push(S, exp[i], 0, 0);
+
+        }else if(exp[i] == ')' || exp[i] == ']' || exp[i] == '}'){
+            while(_get_top(S, &data) && (data != '(' && data != '[' && data != '{')){
+                ans[j] = data;
+                j++;
+                _pop(S, &data, &tmp);
+            }
+            if(data == '(' || data == '[' || data == '{')
+                _pop(S, &data, &tmp);
+        
+        }else{
+            /// exp[i] eh operando
+            ans[j] = exp[i];
+            j++;
+        }
+    }
+
+    while(_pop(S, &data, &tmp)){
+        ans[j] = data;
+        j++;
+    }
+    ans[j] = '\0';
+    strcpy(exp, ans);
 }
 
-void _print(Stack S){
-    while(S != NULL){
-        printf("[%c] ", S->data);
-        S = S->next;
+int _solve_post_fix(Stack *S, char pos[], float *r){
+    /// return 0 se estiver faltando operandos, -1 se estiver faltando operadores, 1 sucesso.
+    /// expressao tem que estar na forma pos-fixa e os operandos tem
+    /// que ser digitos (0,1,...,9).
+    char op;
+    float x1, x2;
+    for(int i = 0; pos[i] != '\0'; i++){
+        if(isdigit(pos[i])){
+            _push(S, '0', (int)(pos[i]-'0'), 1);
+        
+        }else{
+            /// pos[i] == operador
+            op = pos[i];
+            if(!_pop(S, &op, &x2) || !_pop(S, &op, &x1))
+                return 0;
+            
+            if(op == '+')
+                *r = x1 + x2;
+            else if(op == '-')
+                *r = x1 - x2;
+            else if(op == '*')
+                *r = x1 * x2;
+            else if(op == '/'){
+                if(x2 == 0) 
+                    return 2; /// nao pode divisao por 0
+                *r = x1 / x2;
+            }
+            else if(op == '^')
+                *r = pow(x1, x2);
+
+            _push(S, '0', *r, 1);
+        }
     }
-    printf("\n");
+
+    _pop(S, &op, r);
+    if(_empty_stack(*S))
+        return 1;
+    else
+        return -1;
+}
+
+void _print(Stack node){
+    /// node == ponteiro para o topo da stack
+    if(_empty_stack(node))
+        _empty_msg();
+    while(node != NULL){
+        /// se key == 1, node armazena um float, informar funcao print que sera printado
+        /// um float.
+        if(node->key)
+            _print_elem('0', *((float*)node->data), 1);
+        else
+            _print_elem(*((char*)node->data), 0, 0);
+        node = node->next;
+    }
 }
